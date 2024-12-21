f90d74ef9a39fb80d3c6264486de274324843097
Dipan Srimany <dipansrimany@gmail.com>
2024-12-21 20:37:44 +0530

    db log updated

--- src/db/index.js
+++ src/db/index.js
@@ -4,7 +4,7 @@ import {DB_Name} from "../lib/constants";
  export default async function connentDB(){
 try {
     await mongoose.connect(`${process.env.MONGO_URI || ""}/${DB_Name}`);
-    console.log(`Database is connected !!! at PORT: ${process.env.PORT}`);
+    console.log(`Database is connected at PORT: ${process.env.PORT}`);
     
 } catch (error) {
     console.log(`Error: ${error}`);
